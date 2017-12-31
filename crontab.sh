9ef011f0a7edc446b75087094ae16064d3b0106b
Drew Holt <drewderivative@gmail.com>
2017-12-31 12:57:12 -0700

    fixing for travis-ci shellcheck

--- crontab.sh
+++ crontab.sh
@@ -1,8 +1,9 @@
-# shellcheck disable=SC2035,SC2046,SC2086,SC2148
+# shellcheck disable=SC2035,SC2046,SC2086,SC2148,SC2211
 # SC2035: Use ./*glob* or -- *glob* so names with dashes won't become options.
 # SC2046: Quote this to prevent word splitting.
 # SC2086: Double quote to prevent globbing and word splitting.
 # SC2148: Tips depend on target shell and yours is unknown. Add a shebang.
+# SC2211: This is a glob used as a command name. Was it supposed to be in ${..}, array, or is it missing quoting?
 
 0 3,15 * * * certbot renew --quiet
 0 6 * * * DIR=/var/www/drew.invadelabs.com/sitemap; mv $DIR/sitemap-drew.invadelabs.com-NS_0-0.xml $DIR/sitemap-drew.invadelabs.com-NS_0-0.$(date '+\%Y\%m\%d\%H\%M\%S').xml; php /var/www/drew.invadelabs.com/maintenance/generateSitemap.php --compress no --fspath=/var/www/drew.invadelabs.com/sitemap/ --identifier=drew.invadelabs.com --urlpath=https://drew.invadelabs.com/ --server=https://drew.invadelabs.com > /dev/null
