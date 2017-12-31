21f1b05d1e1d1a8eb83e29381b812e1070913cef
Drew Holt <drewderivative@gmail.com>
2017-12-31 12:14:39 -0700

    linted with shellcheck

--- mysql_backup.sh
+++ mysql_backup.sh
@@ -3,11 +3,11 @@ USER=
 PW=
 DIR=/mnt/raid5/drew/backup/mysql
 
-echo Running: `date`;
+echo Running: "$(date '+%Y%m%d%H%M%S')";
 
 mkdir -vp $DIR;
 
-mysqldump -u$USER -p$PW --databases drewwiki mysql | gzip > $DIR/mysql.`hostname -s`.`date +%F.%T`.sql.gz;
+mysqldump -u"$USER" -p"$PW" --databases drewwiki mysql | gzip > "$DIR"/mysql."$(hostname -s)"."$(date '+%Y%m%d%H%M%S')".sql.gz;
 #mysqldump -u$USER -p$PW --all-databases | gzip > $DIR/mysql.`hostname -s`.`date +%F.%T`.sql.gz;
 
 # delete backups older than 30 days
