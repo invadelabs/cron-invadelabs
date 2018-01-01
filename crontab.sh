1c44040018fc9e4e899e5e9ebdbb5c5725860b32
Drew Holt <drewderivative@gmail.com>
2018-01-01 00:50:14 -0700

    set hostname every due to gcp changing it

--- crontab.sh
+++ crontab.sh
@@ -8,4 +8,4 @@
 0 3,15 * * * certbot renew --quiet
 0 6 * * * DIR=/var/www/drew.invadelabs.com/sitemap; mv $DIR/sitemap-drew.invadelabs.com-NS_0-0.xml $DIR/sitemap-drew.invadelabs.com-NS_0-0.$(date '+\%Y\%m\%d\%H\%M\%S').xml; php /var/www/drew.invadelabs.com/maintenance/generateSitemap.php --compress no --fspath=/var/www/drew.invadelabs.com/sitemap/ --identifier=drew.invadelabs.com --urlpath=https://drew.invadelabs.com/ --server=https://drew.invadelabs.com > /dev/null
 18 8 * * * DATE=$(date '+\%Y\%m\%d\%H\%M\%S'); php /var/www/drew.invadelabs.com/maintenance/sqlite.php -q --backup-to /tmp/drew_wiki."$DATE".sqlite; tar -cJf /tmp/invadelabs.com.$DATE.tar.xz -C /etc apache2/ -C /etc letsencrypt/ -C /tmp drew_wiki."$DATE".sqlite; echo "invadelabs.com backup $DATE" | mailx -A /tmp/invadelabs.com.$DATE.tar.xz -s "invadelabs.com backup $DATE" drewderivative@gmail.com; rm /tmp/invadelabs.com."$DATE".tar.xz /tmp/drew_wiki."$DATE".sqlite
-* 1,7,13,19 * * * hostname invadelabs.com
+0 * * * * hostname invadelabs.com
