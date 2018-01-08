8a66dba0b4eed49702124b764fec74bc7c40060d
Drew Holt <drewderivative@gmail.com>
2018-01-08 10:54:24 -0700

    create one .tar.xz instead of multiple files

--- dd-wrt_backup.sh
+++ dd-wrt_backup.sh
@@ -3,19 +3,21 @@
 IP_ADDR="192.168.1.1"
 USER=""
 PASS=""
-
+DATE=$(date '+%Y%m%d%H%M%S')
 LOGFILE="/home/drew/cron/dd-wrt_backup.log"
-CFEFILE="/mnt/raid5/drew/backup/dd-wrt/cfe.bin-$(date '+%Y%m%d%H%M%S')";
-NVRAMBAKFILE="/mnt/raid5/drew/backup/dd-wrt/nvrambak.bin-$(date '+%Y%m%d%H%M%S')";
+DIR="/mnt/raid5/drew/backup/dd-wrt"
+CFEFILE="$DIR/cfe.bin-$DATE";
+NVRAMBAKFILE="$DIR/nvrambak.bin-$DATE";
+TRAFFDATA="$DIR/traffdata.bak-$DATE";
 
 wget -a $LOGFILE --user=$USER --password=$PASS \
 	http://$IP_ADDR/backup/cfe.bin -O "$CFEFILE";
-gzip "$CFEFILE";
 
 wget -a $LOGFILE --user=$USER --password=$PASS \
 	http://$IP_ADDR/nvrambak.bin -O "$NVRAMBAKFILE";
-gzip "$NVRAMBAKFILE";
 
 wget -a $LOGFILE --user=$USER --password=$PASS \
-	http://$IP_ADDR/traffdata.bak -O- | gzip > \
-/mnt/raid5/drew/backup/dd-wrt/traffdata.bak."$(date '+%Y%m%d%H%M%S')".gz;
+	http://$IP_ADDR/traffdata.bak -O "$TRAFFDATA";
+
+tar -cJvf $DIR/dd-wrt-"$DATE".tar.xz "$CFEFILE $TRAFFDATA $NVRAMBAKFILE";
+rm "$CFEFILE $TRAFFDATA $NVRAMBAKFILE";
