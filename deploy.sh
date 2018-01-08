b1b829bb7038c114b6e31d951ebca4bd8fc9f34b
Drew Holt <drewderivative@gmail.com>
2018-01-08 10:59:21 -0700

    update email address in for crontab.sh backup

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-REPO_DIR="/home/drew/invadelabs.com"; git -C "$REPO_DIR" pull; rsync -av --delete --exclude .git/ "$REPO_DIR"/ /var/www/invadelabs.com/ | mailx -s "invadelabs.com $(git -C "$REPO_DIR" log -1 --decorate=no | head -1) deployed to site" drewderivative@gmail.com
+REPO_DIR="/home/drew/invadelabs.com"; git -C "$REPO_DIR" pull; rsync -av --delete --exclude .git/ "$REPO_DIR"/ /var/www/invadelabs.com/ | mailx -s "invadelabs.com $(git -C "$REPO_DIR" log -1 --decorate=no | head -1) deployed to site" drew@invadelabs.com
