98132954f222c22e43044dca0ed772d937eaa8de
Drew Holt <drewderivative@gmail.com>
2018-02-17 15:35:20 -0700

    re-wrote with functions to be easier to read

--- mysql_backup.sh
+++ mysql_backup.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+# mysql_backup.sh
+# Drew Holt <drewderivative@gmail.com>
+# Simple mysql backup script
+
 USER=
 PW=
 DIR=/backup/mysql
@@ -6,7 +10,18 @@ DATE=$(date '+%Y%m%d%H%M%S')
 
 echo Running: "$DATE";
 
-mysqldump -u"$USER" -p"$PW" --databases drewwiki mysql | gzip > "$DIR"/mysql."$(hostname -s)"."$DATE".sql.gz;
+# dump databases drewwiki and mysql
+dump_sql () {
+  mysqldump -u"$USER" -p"$PW" --databases drewwiki mysql
+}
+
+# create xz archive with hostname and date run
+create_archive () {
+  xz > "$DIR"/mysql."$(hostname -s)"."$DATE".sql.xz
+}
+
+# put it together
+dump_sql | create_archive
 
 # delete backups older than 30 days
 # find $DIR -type f -mtime +30 -exec rm -v {} \;
