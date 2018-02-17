c56509ebe5de13ca079008d83a824707e6839236
Drew Holt <drewderivative@gmail.com>
2018-02-17 15:43:28 -0700

    added variables in hostname_test.sh

--- hostname_test.sh
+++ hostname_test.sh
@@ -2,13 +2,17 @@
 # not for cron but check how often google cloud resets hostname
 # nohup hostname_test.sh
 
+HOST_CHECK="invadelabs.com"
+DATE=$(date '+%Y%m%d%H%M%S')
+CHECK_FILE="/tmp/hostname_test-$DATE.log"
+
 while true;
   do
-  if [ "$(hostname -f)" == "invadelabs.com" ]; then
-    echo "$(date)" "$(hostname)" >> /home/drew/hostname_test
+  if [ "$(hostname -f)" == "$HOST_CHECK" ]; then
+    echo "$(date)" "$(hostname)" >> "$CHECK_FILE"
     sleep 1
   else
-    echo "$(date)" "$(hostname)" >> /home/drew/hostname_test
+    echo "$(date)" "$(hostname)" >> "$CHECK_FILE"
     break
   fi
 done
