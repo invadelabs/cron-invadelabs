6dbd001e4f18d0bff9b99dbddfdd8e424a8574af
Drew Holt <drewderivative@gmail.com>
2018-02-17 15:19:37 -0700

    consolidate wgets

--- dd-wrt_backup.sh
+++ dd-wrt_backup.sh
@@ -11,12 +11,8 @@ NVRAMBAKFILE="$DIR/nvrambak.bin-$DATE";
 TRAFFDATA="$DIR/traffdata.bak-$DATE";
 
 wget -a $LOGFILE --user=$USER --password=$PASS \
-	http://$IP_ADDR/backup/cfe.bin -O "$CFEFILE";
-
-wget -a $LOGFILE --user=$USER --password=$PASS \
-	http://$IP_ADDR/nvrambak.bin -O "$NVRAMBAKFILE";
-
-wget -a $LOGFILE --user=$USER --password=$PASS \
+	http://$IP_ADDR/backup/cfe.bin -O "$CFEFILE" \
+	http://$IP_ADDR/nvrambak.bin -O "$NVRAMBAKFILE" \
 	http://$IP_ADDR/traffdata.bak -O "$TRAFFDATA";
 
 tar -cJvf $DIR/dd-wrt-"$DATE".tar.xz "$CFEFILE $TRAFFDATA $NVRAMBAKFILE";
