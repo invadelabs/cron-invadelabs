f991ec841a8d484683acce41d0da7790067976b8
Drew Holt <drew-holt@users.noreply.github.com>
2018-03-24 22:03:23 -0600

    run lynis once a week, don't run jobs concurrently (#21)

--- crontab-invadelabs.com.sh
+++ crontab-invadelabs.com.sh
@@ -4,7 +4,7 @@
 
 # m h  dom mon dow   command
 0 3,15 * * * certbot --apache renew --quiet
-0 6 * * * /root/gen_sitemap.sh -s
-0 6 * * * /root/gdrive_backup.sh -a invadelabs.com -d /snap/bin -f Backup/Web -l gdrive_backup_invadelabs.com.txt -s
+0 5 * * * /root/gen_sitemap.sh -s
+1 5 * * * /root/gdrive_backup.sh -a invadelabs.com -d /snap/bin -f Backup/Web -l gdrive_backup_invadelabs.com.txt -s
 0 * * * * hostname invadelabs.com
-0 6 * * * /usr/sbin/lynis audit system | /root/ansi2html.sh --bg=dark | mailx -a 'Content-Type: text/html' -s "Lynis Audit: invadelabs.com" drewderivative@gmail.com
+59 4 * * 0 /usr/sbin/lynis audit system | /root/ansi2html.sh --bg=dark | mailx -a 'Content-Type: text/html' -s "Lynis Audit: invadelabs.com" drewderivative@gmail.com
