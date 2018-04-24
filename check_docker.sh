478e65a7accecffe1d74dfc0b908fac2179aedf6
Drew Holt <drewderivative@gmail.com>
2018-04-23 19:42:32 -0600

    GH-28 use case instead of if/elif

--- check_docker.sh
+++ check_docker.sh
@@ -4,18 +4,19 @@
 # Slack one message once it's running state is true
 # /root/check_docker.sh
 
-STATUS="$(docker inspect -f '{{.State.Running}}' nagios4)"
+CONTAINER="nagios4"
+STATUS="$(docker inspect -f '{{.State.Running}}' $CONTAINER)"
 IP="$(hostname -I | cut -f1 -d" ")"
-DOCKER_STATUS="$(docker ps -a | grep nagios4)"
+DOCKER_STATUS="$(docker ps -a | grep $CONTAINER)"
 LOCK_FILE="/tmp/docker.down"
 
 send_msg () {
-  echo "$1" | \
+  echo "Container $CONTAINER is $1 on $HOSTNAME" | \
   /root/slacktee.sh \
   --config /root/slacktee.conf \
   -e "docker ps -a" "$DOCKER_STATUS "\
-  -t "Nagios $2 on $HOSTNAME" \
-  -a "$3" \
+  -t "Container $CONTAINER is $1 on $HOSTNAME" \
+  -a "$2" \
   -p \
   -c alerts \
   -u "$(basename "$0")" \
@@ -23,22 +24,26 @@ send_msg () {
   -l http://"$IP":8080 > /dev/null;
 }
 
-if [ "$STATUS" == "false" ]; then
-  if [ ! -f $LOCK_FILE ]; then
-    MESSAGE="Nagios container not running on $HOSTNAME."
-    STATE="down"
-    COLOR="danger"
-    send_msg "$MESSAGE" "$STATE" "$COLOR"
-    # create a lock file to only alert once
-    touch "$LOCK_FILE"
-  fi
-elif [ "$STATUS" == "true" ]; then
-  if [ -f $LOCK_FILE ]; then
-    MESSAGE="Nagios container running on $HOSTNAME."
-    STATE="up"
-    COLOR="good"
-    send_msg "$MESSAGE" "$STATE" "$COLOR"
-    # remove the lock file
-    rm "$LOCK_FILE"
-  fi
-fi
+case "$STATUS" in
+  false)
+    if [ ! -f $LOCK_FILE ]; then
+      MESSAGE="not running"
+      COLOR="danger"
+      send_msg "$MESSAGE" "$COLOR"
+      # create a lock file to only alert once
+      touch "$LOCK_FILE"
+    fi
+    ;;
+  true)
+    if [ -f $LOCK_FILE ]; then
+      MESSAGE="running"
+      COLOR="good"
+      send_msg "$MESSAGE" "$COLOR"
+      # remove lock file
+      rm "$LOCK_FILE"
+    fi
+    ;;
+  *)
+    echo "$STATUS $DOCKER_STATUS"
+    exit 1
+esac
