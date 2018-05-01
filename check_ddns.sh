5a245f604bb16b363d6348a380bbef46986e00d6
Drew Holt <drewderivative@gmail.com>
2018-05-01 07:57:28 -0600

    invadelabs/ansible-invadelabs#5 re-wrote check_ddns.sh and updated for ansible

--- check_ddns.sh
+++ check_ddns.sh
@@ -1,10 +1,34 @@
 #!/bin/bash
 # Drew Holt <drew@invadelabs.com>
 
-USER="$(head -1 /root/check_ddns.txt)"
-PASS="$(tail -1 /root/check_ddns.txt)"
-IP="$(dig +short myip.opendns.com @resolver1.opendns.com)"
 DDNS_HOST=parents.invadelabs.com
 
-curl -sS "https://$USER:$PASS@domains.google.com/nic/update?hostname=$DDNS_HOST&myip=$IP" | \
-  xargs echo "$(date '+%Y%m%d%H%M%S%z')" >> google_ddns.log
+IP="$(dig +short myip.opendns.com @resolver1.opendns.com)"
+DDNS_IP="$(dig +short @8.8.8.8 $DDNS_HOST)"
+DATE="$(date '+%Y-%m-%d-%H:%M:%S%z')"
+
+slack_msg () {
+  if [ ! -f /root/scripts/slacktee.sh ]; then
+    curl -sS -o /root/scripts/slacktee.sh https://raw.githubusercontent.com/course-hero/slacktee/master/slacktee.sh
+    chmod 755 /root/scripts/slacktee.sh
+  fi
+  echo "$1" | \
+  /root/scripts/slacktee.sh \
+  --config /root/slacktee.conf \
+  -a warning \
+  -c general \
+  -u "$(basename "$0")" \
+  -i ipv4 \
+  -t "$DDNS_HOST at $IP" > /dev/null;
+  # -e "docker ps -a" "$DOCKER_STATUS "\
+  # -t "Container $CONTAINER is $1 on $HOSTNAME" \
+  # -p \
+}
+
+if [ ! "$IP" == "$DDNS_IP" ]; then
+  CURL="$(curl --netrc-file /root/check_ddns.cred -sS "https://domains.google.com/nic/update?hostname=$DDNS_HOST&myip=$IP")"
+
+  slack_msg "$DDNS_HOST IP updated on $DATE with $CURL"
+fi
+
+# https://user:pass@domains.google.com/nic/update?hostname=$DDNS_HOST&myip=$IP
