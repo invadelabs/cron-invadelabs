66e47896a6ad86113de52769fe6d004fcb32d6de
Drew Holt <drewderivative@gmail.com>
2018-05-01 00:27:54 -0600

    GH-36 dl slacktee.sh silently if it doesn't exist

--- check_docker.sh
+++ check_docker.sh
@@ -12,7 +12,7 @@ LOCK_FILE="/tmp/docker.down"
 
 slack_msg () {
   if [ ! -f /root/scripts/slacktee.sh ]; then
-    curl -o /root/scripts/slacktee.sh https://raw.githubusercontent.com/course-hero/slacktee/master/slacktee.sh
+    curl -sS -o /root/scripts/slacktee.sh https://raw.githubusercontent.com/course-hero/slacktee/master/slacktee.sh
     chmod 755 /root/scripts/slacktee.sh
   fi
   echo "Container $CONTAINER is $1 on $HOSTNAME" | \
