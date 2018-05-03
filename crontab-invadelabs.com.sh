d6db02092454324f77e424c19ac906d6d57564df
Drew Holt <drewderivative@gmail.com>
2018-05-02 21:56:37 -0600

    invadelabs/ansible-invadelabs#5 setup cron for ansible

--- crontab-invadelabs.com.sh
+++ crontab-invadelabs.com.sh
@@ -3,8 +3,8 @@
 # SC2148: Tips depend on target shell and yours is unknown. Add a shebang.
 
 # m h  dom mon dow   command
-0 3,15 * * * certbot --apache renew --quiet
+#0 3,15 * * * certbot --apache renew --quiet
 0 5 * * * /root/scripts/gen_sitemap.sh -s
-1 5 * * * /root/scripts/gdrive_backup.sh -a invadelabs.com -d /snap/bin -f Backup/Web -l gdrive_backup_invadelabs.com.txt -s
+1 5 * * * /root/scripts/gdrive_backup.sh -a invadelabs.com -d /snap/bin -f Backup/Web -l /root/scripts/gdrive_backup_invadelabs.com.txt -s
 0 * * * * hostname invadelabs.com
 59 4 * * 0 /root/scripts/check_lynis.sh
