5338f705da55d0e5194ea6a48d160e5072df478e
Drew Holt <drewderivative@gmail.com>
2019-06-29 18:04:14 -0600

    update es container names

--- check_docker.sh
+++ check_docker.sh
@@ -6,7 +6,7 @@
 # Slack one message if container isn't running
 # Slack one message when the container is running again
 
-LIST="nagios4 bind dockerelk_logstash docker-elk_logstash docker-elk_kibana docker-elk_elasticsearch nightscout"
+LIST="nagios4 bind docker-elk_logstash docker-elk_kibana docker-elk_elasticsearch nightscout"
 
 slack_msg () {
   if [ ! -f /root/scripts/slacktee.sh ]; then
