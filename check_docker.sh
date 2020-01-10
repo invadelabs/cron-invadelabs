2cc26baebb57da0ccae9370e8f1278638e450a44
Drew Holt <drewderivative@gmail.com>
2020-01-09 17:21:07 -0700

    remove elasticsearch for now due to container renames

--- check_docker.sh
+++ check_docker.sh
@@ -6,7 +6,8 @@
 # Slack one message if container isn't running
 # Slack one message when the container is running again
 
-LIST="nagios4 bind docker-elk_logstash_1 docker-elk_kibana_1 docker-elk_elasticsearch_1 nightscout"
+#LIST="nagios4 bind docker-elk_logstash_1 docker-elk_kibana_1 docker-elk_elasticsearch_1 nightscout"
+LIST="nagios4 bind nightscout"
 
 slack_msg () {
   if [ ! -f /root/scripts/slacktee.sh ]; then
