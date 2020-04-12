64f5e6d58ce9d079bb076e30eedcff8472d1b9cd
Drew Holt <drewderivative@gmail.com>
2020-04-12 13:02:06 -0600

    pass username from script input and password from file

--- dd-wrt_backup.sh
+++ dd-wrt_backup.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 # Drew Holt <drew@invadelabs.com>
 # script to backup dd-wrt; cfe.bin nvrambak.bin traffdata.bak
+# e.x. ./dd-wrt_back.up admin /path/to/pw
 
 IP_ADDR="192.168.1.1"
-USER=""
-PASS=""
+USER=$1
+PASS=$(cat "$2")
 
 DATE=$(date '+%Y%m%d%H%M%S%z')
 DIR="/backup/dd-wrt"
@@ -13,10 +14,10 @@ CFEFILE="$DIR/cfe.bin-$DATE";
 NVRAMBAKFILE="$DIR/nvrambak.bin-$DATE";
 TRAFFDATA="$DIR/traffdata.bak-$DATE";
 
-wget -a $LOGFILE --user=$USER --password=$PASS \
-	http://$IP_ADDR/backup/cfe.bin -O "$CFEFILE" \
-	http://$IP_ADDR/nvrambak.bin -O "$NVRAMBAKFILE" \
-	http://$IP_ADDR/traffdata.bak -O "$TRAFFDATA";
+wget -a "$LOGFILE" --user="$USER" --password="$PASS" \
+	http://"$IP_ADDR"/backup/cfe.bin -O "$CFEFILE" \
+	http://"$IP_ADDR"/nvrambak.bin -O "$NVRAMBAKFILE" \
+	http://"$IP_ADDR"/traffdata.bak -O "$TRAFFDATA";
 
 tar -cJvf $DIR/dd-wrt-"$DATE".tar.xz "$CFEFILE $TRAFFDATA $NVRAMBAKFILE";
 rm "$CFEFILE $TRAFFDATA $NVRAMBAKFILE";
